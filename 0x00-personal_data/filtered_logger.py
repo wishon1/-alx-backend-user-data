6a01b07cbacd61e8b0afa57204bc82edfda22f2a
wishon1 <wisdomhonest1@gmail.com>
2024-05-31 12:03:55 -0400

    add task filter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,28 +9,6 @@ from typing import List
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str],
-                 redaction: str,
-                 message: str,
-                 separator: str) -> str:
-    """
-    function called filter_datum that returns the log message obfuscated:
-    Arguments:
-        1. fields: a list of strings representing all fields to obfuscate
-        2. redaction: a string representing by what the field will be
-                    obfuscated
-        3. message: a string representing the log line
-        4. separator: a string representing by which character is
-                    separating all fields in the log line (message)
-
-    Returns:  returns the log message obfuscated:
-    """
-    for string in fields:
-        re_pattern = re.sub(f'{string}=.*?{separator}',
-                            f'{string}={redaction}{separator}', message)
-    return re_pattern
-
-
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class """
 
@@ -51,3 +29,25 @@ class RedactingFormatter(logging.Formatter):
         original_msg = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION, original_msg,
                             self.SEPARATOR)
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """
+    function called filter_datum that returns the log message obfuscated:
+    Arguments:
+        1. fields: a list of strings representing all fields to obfuscate
+        2. redaction: a string representing by what the field will be
+                    obfuscated
+        3. message: a string representing the log line
+        4. separator: a string representing by which character is
+                    separating all fields in the log line (message)
+
+    Returns:  returns the log message obfuscated:
+    """
+    for string in fields:
+        re_pattern = re.sub(f'{string}=.*?{separator}',
+                            f'{string}={redaction}{separator}', message)
+    return re_pattern
