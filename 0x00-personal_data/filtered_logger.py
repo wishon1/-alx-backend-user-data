e3049f0e2bc1239a4895662f3c1200b97eef82ff
wishon1 <wisdomhonest1@gmail.com>
2024-05-31 13:13:04 -0400

    add task 3 and 4 to filter_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -51,3 +51,55 @@ def filter_datum(fields: List[str],
         re_pattern = re.sub(f'{string}=.*?{separator}',
                             f'{string}={redaction}{separator}', message)
     return re_pattern
+
+
+def get_logger() -> logging.Logger:
+    """function that takes no arguments and returns a logging.Logger object."""
+    logger  = logging.getlogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+    return logger.addHandler(stream_handler)
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """returns a connection to the database"""
+    user_name = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    user_password = os.getenv('PERSONAL_DATA_HOST', 'localhost')
+    db_host = os.getenv('PERSONAL_DATA_HOST', 'localhost')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    if not user_name:
+        raise ValueError("name must be set in the environment variable")
+
+    connection_seesion = mysql.connection.connect(
+        user=user_name,
+        password=user_password,
+        host=db_host,
+        database=db_name
+    )
+    return connection_session
+
+
+def main():
+    """connect to the database"""
+    db_connection = get_db()
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    logger = get_logger()
+    for row in rows:
+        original_message = (
+            f"name={row[0]}; email={row[1]}; phone={row[2]}; ssn={row[3]};"
+            f"password={row[4]}; ip={row[5]}; last_login={row[6]};"
+            f" user_agent={row[7]};"
+        )
+        logger.info(original_message)
+    cursor.close()
+    db_connection.close()
+
+
+if __name__ == "__main__":
+    main()
