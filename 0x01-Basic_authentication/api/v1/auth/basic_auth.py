d91898576168a1eb5a7f302e727740060f0d6d1d
wishon1 <wisdomhonest1@gmail.com>
2024-06-04 14:59:36 -0400

    add task 9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,3 +53,27 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """
+        method that returns returns the user email and password from the Base64
+        decoded value.
+
+        Args:
+            decoded_base64_authorization_heade (str): Base64 decoded string
+
+            Returns:
+                 tuple: user email and password or (None, None) if invalid
+                 input
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+
+        usr_email, usr_password = \
+            decoded_base64_authorization_header.split(':', 1)
+        return usr_email, usr_password
