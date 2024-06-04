ceb5a94297524fa4c69877c4e2c87f869e8db05a
wishon1 <wisdomhonest1@gmail.com>
2024-06-04 15:35:48 -0400

    add task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """module containing the class BasicAuth that inherits from Auth"""
 from api.v1.auth.auth import Auth
+from typing import TypeVar, Tuple
+from models.user import User
 import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -77,3 +80,36 @@ class BasicAuth(Auth):
         usr_email, usr_password = \
             decoded_base64_authorization_header.split(':', 1)
         return usr_email, usr_password
+
+    def user_object_from_credentials(self, user_email: str, user_pwd:
+                                     str) -> TypeVar('User'):
+        """
+        Returns the User instance based on the provided email and password.
+
+        Args:
+            user_email (str): User email
+            user_pwd (str): User password
+
+        Returns:
+            User: User instance if credentials are valid, None otherwise
+        """
+        # Check if user_email or user_pwd is None or not a string
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        # Search for the user by email using the User class method `search`
+        user_list = User.search({'email': user_email})
+        if not user_list:
+            return None
+
+        # Assuming that the search method returns a list of User instances
+        user = user_list[0]
+
+        # Check if the password is valid using the `is_valid_password` method
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        # Return the User instance if everything is valid
+        return user
