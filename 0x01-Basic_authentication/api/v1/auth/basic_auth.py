89c17a5d700d2fe58cf788fa751d3bb9af63c6f1
wishon1 <wisdomhonest1@gmail.com>
2024-06-04 12:55:22 -0400

    add task 6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""module containing the class BasicAuth that inherits from Auth"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Do nothing for now"""
