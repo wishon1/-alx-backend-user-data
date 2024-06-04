30a371c12002baa9b52f6bf38efca582c895b9bd
wishon1 <wisdomhonest1@gmail.com>
2024-06-04 10:02:49 -0400

    add task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,9 +8,25 @@ class Auth:
     """class to manage the API authentication"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """returns False - path and excluded_paths"""
-        if path:
-            return False
+        """Define which routes don't need authentication"""
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        # Nomalize path by ensuring it ends with a slash
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            # Normalize excluded_path by ensuring it ends with a slash
+            if not excluded_path.endswith('/'):
+                excluded_path += '/'
+
+            if path == excluded_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """returns None - request will be the Flask request object"""
