775411165f44c0e5bd7300c286754fb91f40421b
wishon1 <wisdomhonest1@gmail.com>
2024-06-11 09:09:51 -0400

    add task 1

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        save the user to the database.
+
+        Args:
+            email (str): The user email
+            hashed_password (str): the hashed password
+
+        Returns: The newly created user
+        """
+        try:
+            new_usr = User(email=email, hashed_password=hashed_password)
+            self.__session.add(new_usr)
+            self.__session.commit()
+
+        except Exception:
+            self._session.rollback()
+            new_usr = None
+
+        return new_usr
