e25e2b811bca9b81f19d662d854295816f85b36d
wishon1 <wisdomhonest1@gmail.com>
2024-06-11 10:17:37 -0400

    udateto task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -50,7 +51,7 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
         """
-
+        find the user
         """
         if kwargs is None:
             raise InvalidRequestError
