ebb6d8bfbfc227a18a93eb446b63ce092e9d94fd
wishon1 <wisdomhonest1@gmail.com>
2024-06-12 16:04:32 -0400

    add task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Basic Flask app with a single GET route that returns a JSON payload.
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 AUTH = Auth()
@@ -27,6 +27,38 @@ def users() -> str:
         return jsonify({"email": usr.email, "message": "user created"})
     except ValueError as err:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login() -> str:
+    """
+    Handle user login by validating email and password, creating
+    a session, and responding with a session ID cookie if successful.
+
+    Returns:
+        str: A JSON response containing the user's email and a success
+        message if the login is successful. If the login fails, aborts with
+        a 401 status.
+    """
+    # Retrieve email and password from the form data in the request
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    # Validate the login credentials
+    if AUTH.valid_login(email, password):
+        # Create a new session for the user
+        session_id = AUTH.create_session(email)
+
+        # Create a JSON response with the user's email and a success message
+        response = jsonify({"email": email, "message": "logged in"})
+
+        # Set a cookie on the response with the session ID
+        response.set_cookie("session_id", session_id)
+
+        return response
+    else:
+        # Abort with a 401 Unauthorized status if login is invalid
+        abort(401)
 
 
 if __name__ == "__main__":
